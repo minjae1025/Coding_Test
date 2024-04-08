64bbacacef7f2d7a19d2fd4753be8059dfdcb4da
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-04-08 22:23:42 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    if (answer==0) {
+        return 1;
+    }else {
+        return -1;
+    }
+}
