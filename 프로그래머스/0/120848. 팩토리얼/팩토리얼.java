da136f643669b6ad5c2d3790d4372e875aeb90c1
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-06-04 22:14:15 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int temp = 1;
+        for (int i = 1; i<=10; i++) {
+            temp *= i;
+            if (temp == n) {
+                answer = i;
+                break;
+            }
+            else if (temp > n) {
+                answer = i-1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
