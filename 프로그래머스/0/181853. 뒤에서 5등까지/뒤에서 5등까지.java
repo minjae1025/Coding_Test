a15054a443f5c8344a3717c1b9231daaf42c4f0e
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-06-10 20:08:42 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        int temp = num_list[0];
+        for (int i = 0; i < num_list.length; i++) {
+            for (int j = i; j < num_list.length; j++) {
+			    if (num_list[i] > num_list[j]) {
+				    temp = num_list[j];
+				    num_list[j] = num_list[i];
+				    num_list[i] = temp;
+			    }
+		    }
+	    }
+        for (int i = 0; i<5; i++) {
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
