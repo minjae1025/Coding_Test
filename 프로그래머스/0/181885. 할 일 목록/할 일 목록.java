c540c4987b3874d3087357e6cdce3ede18ea5391
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-06-15 22:51:58 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        int cnt = 0;
+        for (int i = 0; i<finished.length; i++) {
+            if (finished[i] == false) {
+                cnt++;
+            }
+        }
+        String[] answer = new String[cnt];
+        cnt = 0;
+        for (int i = 0; i<todo_list.length; i++) {
+            if (finished[i] == false) {
+                answer[cnt] = todo_list[i];
+                cnt++;
+            }
+        }
+        return answer;
+    }
+}
