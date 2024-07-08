910c5741e74eff50f276eed9810f7401727a612f
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-07-08 13:54:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int temp = x;
+        int sum = 0;
+        while (temp>0) {
+            sum += temp%10;
+            temp /= 10;
+        }
+        if (x % sum == 0) {
+            answer = true;
+        }
+        else {
+            answer = false;
+        }
+        return answer;
+    }
+}
