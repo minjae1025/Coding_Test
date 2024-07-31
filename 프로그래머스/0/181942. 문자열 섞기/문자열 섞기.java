5769d3de7724d08c66bc2830b10aad42f1e28e73
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-08-01 01:41:19 +0900

    [level 0] Title: 문자열 섞기, Time: 15.24 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        int length = str1.length()+str2.length();
+        for (int i = 0; i<length; i++) {
+            if (i%2==0) {
+                answer += str1.charAt(i/2);
+            }
+            else {
+                answer += str2.charAt(i/2);
+            }
+        }
+        return answer;
+    }
+}
