63a5a03d6b7bbe75d4f62d3f9a59e0a1ad2a2ba0
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-08-30 07:53:07 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 14.63 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int[] alp = new int[26];
+        for (int i = 0; i<s.length(); i++) {
+            alp[s.charAt(i)-'a']++;
+        }
+        for (int i = 0; i<alp.length; i++) {
+            if (alp[i] == 1) {
+                answer += (char)(i+'a');
+            }
+        }
+        return answer;
+    }
+}
