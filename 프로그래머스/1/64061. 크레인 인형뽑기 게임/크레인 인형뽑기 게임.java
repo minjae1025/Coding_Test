3b5868dfb3ea3abddbd3ebc86d6c49470184fbab
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-09-24 23:47:22 +0900

    Update 크레인 인형뽑기 게임.java

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         int[] idx = new int[board.length];
         int[] cnt = new int[moves.length];
-        int top = -1; // 스택의 최상단을 가리키는 인덱스
+        int top = 0; // 스택의 최상단을 가리키는 인덱스
         
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
@@ -17,7 +17,7 @@ class Solution {
         for (int i = 0; i < moves.length; i++) {
             int column = moves[i] - 1;
             if (idx[column] < board.length && board[idx[column]][column] > 0) {
-                cnt[++top] = board[idx[column]][column];
+                cnt[top] = board[idx[column]][column];
                 board[idx[column]][column] = 0;
                 idx[column]++;
                 
@@ -25,6 +25,7 @@ class Solution {
                     answer += 2;
                     top -= 2;
                 }
+                top++;
             }
         }
         
