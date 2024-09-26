5143c1684887223886c9a864591e7e53c5905ef3
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-09-26 19:50:56 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.78 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,9 +4,8 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         int[] k_s = new int[k];
-        int min = 2000;
+        
         for (int i = 0; i<score.length; i++) {
-            
             if (i<k) {
                 k_s[i] = score[i];
             }
@@ -15,6 +14,7 @@ class Solution {
                     k_s[0] = score[i];
                 }   
             }
+            
             if (i < k-1) {
                 Arrays.sort(k_s, 0, i+1);
                 answer[i] = k_s[0];
