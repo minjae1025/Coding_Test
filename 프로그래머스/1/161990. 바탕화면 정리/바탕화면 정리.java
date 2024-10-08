a1bcde13e511d78a3b17625dfb73a1eb90d02ca1
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-10-08 23:56:54 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.19 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0};
+        for (int i = 0; i<wallpaper.length; i++) {
+            for (int j = 0; j<wallpaper[i].length(); j++) {
+                char temp = wallpaper[i].charAt(j);
+                if(temp == '#') {
+                    if (i < answer[0]) {
+                        answer[0] = i;
+                    }
+                    if (j < answer[1]) {
+                        answer[1] = j;
+                    }
+                    if (i+1 > answer[2]) {
+                        answer[2] = i+1;
+                    }
+                    if (j+1 > answer[3]) {
+                        answer[3] = j+1;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
