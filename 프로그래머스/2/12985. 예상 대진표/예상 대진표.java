f30f493e1a9d6fb127ba1f8e07dd32a3a44f555f
minjae1025 <92581525+minjae1025@users.noreply.github.com>
2024-10-15 23:16:49 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int N, int A, int B) {
+        int answer = 0;
+
+        while (A != B) {
+            A = (A + 1) / 2;
+            B = (B + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
